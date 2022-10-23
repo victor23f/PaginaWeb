a2c89e518f2d80076c9997f1a68eff8261eb8be4
victor23f <victor23600ff@gmail.com>
2022-10-23 22:22:45 +0200

    Intent implícito

--- app/src/main/java/com/example/logeoejemplo/MainActivity.java
+++ app/src/main/java/com/example/logeoejemplo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         WebView miWeb = (WebView) findViewById(R.id.pagina);
         miWeb.loadUrl("https://www.google.com");
+
     }
     public void presionarBoton(View view){
         String usuario,contrasena;
@@ -33,7 +35,9 @@ public class MainActivity extends Activity {
 
     }
     public void paginaWeb(View vista){
-
+        Uri webpage = Uri.parse("https://www.google.com");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(webIntent);
 
     }
 }
